8aea756749b3dc1980e2bb951f95a58abe37d63d
yangjeremy <390076563@qq.com>
2018-03-02 12:16:06 +0800

    express-4

---
+++ src/webpack.config.js
@@ -0,0 +1,48 @@
+var webpack = require('webpack')
+var path = require('path')
+
+
+module.exports = {
+    entry: path.join(__dirname, "js/app/index.js"),
+    output: {
+        path: path.join(__dirname, "../public/js"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            {
+            test: /\.less$/,
+            use: ["style-loader","css-loader","less-loader"]
+        }
+       
+    ]
+    },
+    resolve: {
+        alias: {
+            jquery: path.join(__dirname, "js/lib/jquery-2.0.3.min.js"),
+            mod: path.join(__dirname, "js/mod"),
+            less: path.join(__dirname, "less")
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery"
+        }),
+        // new ExtractTextPlugin("css/index.css"),
+        // new webpack.LoaderOptionsPlugin({
+        //     options: {
+        //         postcss: [
+        //             autoprefixer(),
+        //         ]
+        //     }
+        // })
+        // new webpack.optimize.UglifyJsPlugin({
+        //     compress: {
+        //         warnings: false,
+        //     },
+        //     output: {
+        //         comments: false,
+        //     },
+        // }),
+    ]
+};
